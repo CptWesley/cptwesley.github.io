35a33cc29f6c833a949973e7dec3ef1514142a01
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2019-03-10 13:57:30 +0100

    Fix blog list

--- resources/articles/articles.js
+++ resources/articles/articles.js
@@ -5,7 +5,7 @@ function showArticles() {
     requestObject("https://api.github.com/repos/cptwesley/cptwesley.github.io/contents/resources/articles/entries", function(objects) {
         document.getElementById("page").innerHTML = `
             <h1>Blogs</h1>
-            <div id="articles-list-container" class="list-group">
+            <ul id="articles-list-container" class="list-none"></ul>
         `;
         const container = document.getElementById("articles-list-container");
         appendArticleLinkElement(container, objects, 0);
@@ -22,10 +22,12 @@ function showArticles() {
 function appendArticleLinkElement(container, objects, index) {
     const file = getFileNameWithoutExtension(objects[index].path);
     requestObject("/resources/articles/entries/" + file + ".json", function(article) {
-        const item = document.createElement("a");
-        item.setAttribute("href", "javascript:loadPage('/articles/" + file + "');");
+        const item = document.createElement("li");
+        const link = document.createElement("a");
+        link.setAttribute("href", "javascript:loadPage('/articles/" + file + "');");
         const date = new Date(Date.parse(article.date));
-        item.innerText = getDateFormat(date) + " - " + article.title;
+        link.innerText = getDateFormat(date) + " - " + article.title;
+        item.appendChild(link);
         container.appendChild(item);
         if (index < objects.length - 1) {
             appendArticleLinkElement(container, objects, index + 1);
